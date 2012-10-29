375d3db33e922ecc617fdb8e35d71a8bc434efae
Phil Hammond <prhammond@gmail.com>
2012-10-29 17:14:56 -0400

    PRH - pulled all files from piktur server.  created upload.html  pushing all to github so we cen get the server re-synced with github

---
+++ html/js/livevalidation_signup.js
@@ -0,0 +1,21 @@
+var searchterm = new LiveValidation( 'searchterm', { validMessage: " " } );
+searchterm.add( Validate.Format, { pattern: /^[a-z0-9_ ]{1,64}$/, failureMessage: "Invalid search term!" } );
+
+var username = new LiveValidation( 'username', { validMessage: "<" } );
+username.add( Validate.Presence );
+username.add( Validate.Format, { pattern: /^[a-z0-9_]{1,64}$/, failureMessage: "Invalid username!" } );
+
+var email = new LiveValidation( 'email', { validMessage: "<" } );
+email.add( Validate.Presence );
+email.add( Validate.Email, { failureMessage: "Invalid email address!" } );
+email.add( Validate.Format, { pattern: /^[a-z0-9_@\.]{5,64}$/, failureMessage: "Invalid email address!" } );
+
+var email2 = new LiveValidation( 'email2', { validMessage: "<" } );
+email2.add( Validate.Confirmation, { match: 'email', failureMessage: "Email addresses don't match!" } );
+
+var password = new LiveValidation( 'password', { validMessage: "<" } );
+password.add( Validate.Presence );
+password.add( Validate.Format, { pattern: /(?=^[!-~]{8,64}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=^.*[^\s].*$)(?=.*[\d]).*$/, failureMessage: "Invalid password!" } );
+
+var password2 = new LiveValidation( 'password2', { validMessage: "<" } );
+password2.add( Validate.Confirmation, { match: 'password', failureMessage: "Passwords don't match!" } );
