5114f4d91f6ef8efa0ff6d74eadeb1d0173fa4a8
root <root@piktur.poly.edu>
2012-11-10 15:42:30 -0500

    Updated javascript to make validation less intrusive

---
+++ html/js/livevalidation_image_upload.js
@@ -0,0 +1,8 @@
+var searchterm = new LiveValidation( 'searchterm', { validMessage: " " } );
+searchterm.add( Validate.Format, { pattern: /^[a-z0-9_ ]{1,64}$/, failureMessage: "Invalid search term!" } );
+
+var description = new LiveValidation( 'description', { validMessage: "<" } );
+description.add( Validate.Format, { pattern: /^[a-z0-9_ ,]{1,255}$/, failureMessage: "Invalid descrption!" } );
+
+var tags = new LiveValidation( 'tags', { validMessage: "<" } );
+tags.add( Validate.Format, { pattern: /^[a-z0-9_ ,]{1,255}$/, failureMessage: "Invalid tag!" } );
--- html/js/livevalidation_signin.js
+++ html/js/livevalidation_signin.js
@@ -2,9 +2,7 @@ var searchterm = new LiveValidation( 'searchterm', { validMessage: " " } );
 searchterm.add( Validate.Format, { pattern: /^[a-z0-9_ ]{1,64}$/, failureMessage: "Invalid search term!" } );
 
 var username = new LiveValidation( 'username', { validMessage: "<" } );
-username.add( Validate.Presence );
 username.add( Validate.Format, { pattern: /^[a-z0-9_]{1,64}$/, failureMessage: "Invalid username!" } );
 
 var password = new LiveValidation( 'password', { validMessage: "<" } );
-password.add( Validate.Presence );
 password.add( Validate.Format, { pattern: /(?=^[!-~]{8,64}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=^.*[^\s].*$)(?=.*[\d]).*$/, failureMessage: "Invalid password!" } );
