f91d9ef6633a70443668acc8922c825140b4ed52
heyyoulive <heyyoulive@gmail.com>
2012-12-04 20:13:34 -0800

    Issue #69.  Change code to not change email and lock account at time of clicking on change email link on web page.  Continue to send link to email address, then allow user to click on link and change password if desired.

--- html/forgot_password.php
+++ html/forgot_password.php
@@ -7,20 +7,96 @@
   $email = filter_input( INPUT_POST, 'email', FILTER_VALIDATE_EMAIL );
   $securimage = new Securimage();
   $confirm = filter_input( INPUT_GET, 'confirm', FILTER_VALIDATE_REGEXP, array( "options"=>array( "regexp"=>"/^true$/" ) ) );
+  $key = filter_input( INPUT_GET, 'key', FILTER_VALIDATE_REGEXP, array( "options"=>array( "regexp"=>"/^[0-9a-f]{128}$/" ) ) );
+  $password = filter_input( INPUT_POST, 'password', FILTER_VALIDATE_REGEXP, array( "options"=>array( "regexp"=>"/(?=^[!-~]{8,64}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=^.*[^\s].*$)(?=.*[\d]).*$/" ) ) );
+  $fh_flag = filter_input( INPUT_GET, 'fh_flag', FILTER_VALIDATE_REGEXP, array( "options"=>array( "regexp"=>"/^true$/" ) ) );
   
   if ( $confirm ) {
-    $msg = "An email has been sent with a link to re-activate your account.";
+    $msg = "An email has been sent with a link to change your password.";
   }
   
   if ( DEBUG ) {
     echo "EMAIL: ".$email.'<br>';
+    echo "keyS: $key <br>";
   }
   
+  # Issue #69.  Add code to not automatically lock out user, and lock account, if anyone provides as valid email
+  # address.  This new code will send Link to users email, and allow the user to click on the link and change
+  # password.  Note:  this is may first attempt at php coding, so there are probably some bugs.
+  if ( $key )
+  {
+      $email = filter_input( INPUT_GET, 'email', FILTER_VALIDATE_EMAIL );    
+  	  if ( !$email ) {
+          $msg .= "Invalid email address.<br>";
+      } else {
+            #check for email address
+            # Prepare the MySQL select statement on the server
+            if ( !( $stmt = $db->prepare( "SELECT `piktur`.`users`.`forgotten_hash` FROM `piktur`.`users` WHERE `piktur`.`users`.`email_address` = ?;" ) ) ) {
+              die( 'Prepare failed: (' . $db->errno . ') ' . $db->error );
+            } else {
+                # Bind the variables into the prepared statement
+                if ( !$stmt->bind_param( 's', $email ) ) {
+                   die( 'Binding parameters failed: (' . $stmt->errno . ') ' . $stmt->error );
+                } else {
+                    # Execute the SQL command
+                    if ( !$stmt->execute() ) {
+                       die( 'Execute failed: (' . $stmt->errno . ') ' . $stmt->error );
+                    } else {
+                       $stmt->store_result();
+                       $results = $stmt->num_rows;
+                       $stmt->bind_result( $forgotten_hash );
+                       $stmt->fetch();
+                       # Cleanup statement
+                       $stmt->close();
+
+                       $confirm = 'true';
+                       if ( $key == $forgotten_hash ) {
+                          if ( $fh_flag  )
+                          {
+                            $confirm = 'true';
+                            if ( $_POST['password'] ) {
+                                if ( !$password ) {
+                                    $msg .= "Invalid password, your password must be at least 8 characters consisting of at least 1 lowercase character, 1 upercase character, and 1 special character.<br>";
+                                 }
+                                // then store in db.
+                                if ( !( $stmt = $db->prepare( "UPDATE `piktur`.`users` SET `piktur`.`users`.`password_hash` = ? WHERE `piktur`.`users`.`email_address` = ?;" ) ) ) {
+                                    die( 'Prepare failed: (' . $db->errno . ') ' . $db->error );
+                                } else {
+                                   # Bind the variables into the prepared statement
+                                   $password_hash =  hash( 'sha512', $password );
+                                   if ( !$stmt->bind_param( 'ss', $password_hash, $email ) ) {
+                                       die( 'Binding parameters failed: (' . $stmt->errno . ') ' . $stmt->error );
+                                   } else {
+                                   # Execute the SQL command
+                                       if ( !$stmt->execute() ) {
+                                            die( 'Execute failed: (' . $stmt->errno . ') ' . $stmt->error );
+                                        }
+                                        # Cleanup statement
+                                        $stmt->close();
+                                    }
+                                 }  #else prepare
+                                 $fh_flag = 'true';
+                                 $confim = 'true';
+                                 $msg='password change successful!';
+                             } else { die( 'passwd ==TRUE' ); }  #if password == TRUE
+                           } else { #if fh_flag 
+                              $fh_flag = 'true';
+                           }
+                        } else {
+                           $msg='password does not match ';
+                       }
+                  } # if execute
+               } # if bind
+           } #if prepare
+       } #if email
+   } #if key
+                      	
+ 
   #process the form
-  if ( !empty( $_POST ) ) {
-    if ( $securimage->check( $_POST['captcha_code'] ) == FALSE ) {
-      $msg .= "The security code entered was incorrect.<br>";    
-    } else {
+   if (( !empty( $_POST )) && (!$fh_flag)) {
+      if ( $securimage->check( $_POST['captcha_code'] ) == FALSE ) {
+        $msg .= "The security code entered was incorrect.<br>";    
+      } else {
         if ( !$email ) {
           $msg .= "Invalid email address.<br>";
         } else {
@@ -81,14 +157,15 @@
                             $i++;
                           }
                         } 
-                        $password_hash =  hash( 'sha512', $password );
+                        #$password_hash =  hash( 'sha512', $password );
+                        $forgotten_hash = hash( 'sha512', $password );
                         
                         # Update password hash in database and disable the account
-                        if ( !( $stmt = $db->prepare( "UPDATE `piktur`.`users` SET `piktur`.`users`.`password_hash` = ?, `piktur`.`users`.`account_status` = b'0' WHERE `piktur`.`users`.`email_address` = ?;" ) ) ) {
+                        if ( !( $stmt = $db->prepare( "UPDATE `piktur`.`users` SET `piktur`.`users`.`forgotten_hash` = ? WHERE `piktur`.`users`.`email_address` = ?;" ) ) ) {
                           die( 'Prepare failed: (' . $db->errno . ') ' . $db->error );
                         } else {
                             # Bind the variables into the prepared statement
-                            if ( !$stmt->bind_param( 'ss', $password_hash, $email ) ) {
+                            if ( !$stmt->bind_param( 'ss', $forgotten_hash, $email ) ) {
                               die( 'Binding parameters failed: (' . $stmt->errno . ') ' . $stmt->error );
                             } else {
                                 # Execute the SQL command
@@ -114,10 +191,7 @@
                             <td class="logo"><img src="http://piktur.poly.edu/img/piktur.png" alt="PIKTUR Logo"></td>
                           </tr>
                           <tr>
-                            <td class="logo">Your password has been reset at the PIKTUR website. To activate the account, click on the following link to activate the account: <a href="https://piktur.poly.edu/reset_account.php?email='.$email.'&key='.$password_hash.'">Activate Account</a></td>
-                          </tr>
-                          <tr>
-                            <td class="logo">Your new password is '.$password.'.  Please change it once you have activated your account.
+                            <td class="logo">If you have fogotten your password, click on the following link to reset it: <a href="https://piktur.poly.edu/forgot_password.php?email='.$email.'&key='.$forgotten_hash.'">Change Password</a></td>
                           </tr>
                           </table>
                         </body>
@@ -173,7 +247,39 @@
             </tr>
           </table>
         </form>
-      <?php } ?>
+      <?php } elseif ( $fh_flag == 'true') { ?>
+        <form id="add_user_form" name="add_user_form" action="<?php echo $protocol . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'] . '?email='.$email.'&key='.$forgotten_hash.'&fh_flag=true' ?>" method="post">
+      <table border="0" cellpadding="2" cellspacing="2" width="100%">
+        <tbody>
+          <tr>
+            <td colspan="1" rowspan="1" height="200" class="center_top">
+              <table border="0" cellpadding="2" cellspacing="4" width="100%">
+                <tbody>
+                  <tr>
+                    <td class="formlabel" width="50%">Password:</td>
+                    <td class="forminput" width="50%">
+                      <input size="18" name="password" id="password" type="password"<?php if ( $password ) echo " value=\"$password\""; ?>>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="formlabel" width="50%">Confirm Password:</td>
+                    <td class="forminput" width="50%">
+                      <input size="18" name="password2" id="password2" type="password"<?php if ( $password ) echo " value=\"$password\""; ?>>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="1" class="center_middle">
+                <input type="image" src="<?php echo  $protocol . $_SERVER['SERVER_NAME'] ?>/img/submitbutton.gif" height="45" width="125" border="0" alt="Submit Button">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+    <?php   } ?>
       <table border="1" cellpadding="2" cellspacing="2" width="100%">
         <tbody>
           <tr>
