c57ec4f312073ba6f63e8ce7f8ba2297a8b50b8c
Scott C. Kennedy <sck@nogas.org>
2012-12-09 16:40:15 -0800

    Can now delete albums as admin

--- html/delete_album.php
+++ html/delete_album.php
@@ -18,47 +18,89 @@
   $album_array = array(); 
   $albumID_array = array(); 
 
-  if ( isset( $_SESSION['user_id'] ) ) {
-    # Prepare the MySQL select statement on the server
-    if ( !( $stmt = $db->prepare( "SELECT `albums`.`album_id`, `albums`.`album_name` FROM `albums` JOIN `permissions` ON `albums`.`album_id`=`permissions`.`album_id` WHERE `permissions`.`user_id` = ? AND `permissions`.`access_type`='delete';" ) ) ) {
-      die( 'Prepare failed: (' . $db->errno . ') ' . $db->error );
-    } else {
-        # Bind the variables into the prepared statement
-        $start = $page * $albums_per_page;
-        if ( !$stmt->bind_param( 'i', $_SESSION['user_id'] ) ) {
-          die( 'Binding parameters failed: (' . $stmt->errno . ') ' . $stmt->error );
-        } else {
-            # Execute the SQL command
-            if ( !$stmt->execute() ) {
-              die( 'Execute failed: (' . $stmt->errno . ') ' . $stmt->error );
-            } else {
-                # Get total of records in the result set
-                $stmt->store_result();
-                $results = $stmt->num_rows;
-                # Bind results
-                $stmt->bind_result( $album_id, $album_name );
-                # Fetch the values into arrays
-                $i = 0;
-                while ( $stmt->fetch() ) {
-                  $album_array[] = $album_name; 
-                  $albumID_array[] = $album_id;             
-                  # Review results
-                  if ( DEBUG ) {
-                    echo "USER_ID: ".$_SESSION['user_id'].'<br />';
-                    echo "ALBUM_ID[$i]: $album_id<br />";
-                    echo "ALBUM_NAME[$i]: $album_name<br />";
+  if (( isset( $_GET['album'] ) ) && ( $_SESSION['admin_flag'] == 'true' )) {
+     $msg = 'You are an admin and able to delete this album';
+      if ( !( $stmt = $db->prepare( "SELECT `albums`.`album_id`, `albums`.`album_name` FROM `albums` JOIN `permissions` ON `albums`.`album_id`=`permissions`.`album_id` WHERE `albums`.`album_id` = ?;" ) ) ) {
+        die( 'Prepare failed: (' . $db->errno . ') ' . $db->error );
+      } else {
+          # Bind the variables into the prepared statement
+          $start = $page * $albums_per_page;
+          if ( !$stmt->bind_param( 'i', $_GET['album'] ) ) {
+            die( 'Binding parameters failed: (' . $stmt->errno . ') ' . $stmt->error );
+          } else {
+              # Execute the SQL command
+              if ( !$stmt->execute() ) {
+                die( 'Execute failed: (' . $stmt->errno . ') ' . $stmt->error );
+              } else {
+                  # Get total of records in the result set
+                  $stmt->store_result();
+                  $results = $stmt->num_rows;
+                  # Bind results
+                  $stmt->bind_result( $album_id, $album_name );
+                  # Fetch the values into arrays
+                  $i = 0;
+                  while ( $stmt->fetch() ) {
+                    $album_array[] = $album_name; 
+                    $albumID_array[] = $album_id;             
+                    # Review results
+                    if ( DEBUG ) {
+                      echo "USER_ID: ".$_SESSION['user_id'].'<br />';
+                      echo "ALBUM_ID[$i]: $album_id<br />";
+                      echo "ALBUM_NAME[$i]: $album_name<br />";
+                    }
+                    $album_names[$i] = $album_name;
+                    $album_id[$i] = $album_id;
+                    $i++;             
                   }
-                  $album_names[$i] = $album_name;
-                  $album_id[$i] = $album_id;
-                  $i++;             
+                  unset( $i );
+                  # Cleanup statement
+                  $stmt->close();
                 }
-                unset( $i );
-                # Cleanup statement
-                $stmt->close();
-              }
-          }
-      }
-  }
+            }
+        }
+  } else {
+    if ( isset( $_SESSION['user_id'] ) ) {
+      # Prepare the MySQL select statement on the server
+      if ( !( $stmt = $db->prepare( "SELECT `albums`.`album_id`, `albums`.`album_name` FROM `albums` JOIN `permissions` ON `albums`.`album_id`=`permissions`.`album_id` WHERE `permissions`.`user_id` = ? AND `permissions`.`access_type`='delete';" ) ) ) {
+        die( 'Prepare failed: (' . $db->errno . ') ' . $db->error );
+      } else {
+          # Bind the variables into the prepared statement
+          $start = $page * $albums_per_page;
+          if ( !$stmt->bind_param( 'i', $_SESSION['user_id'] ) ) {
+            die( 'Binding parameters failed: (' . $stmt->errno . ') ' . $stmt->error );
+          } else {
+              # Execute the SQL command
+              if ( !$stmt->execute() ) {
+                die( 'Execute failed: (' . $stmt->errno . ') ' . $stmt->error );
+              } else {
+                  # Get total of records in the result set
+                  $stmt->store_result();
+                  $results = $stmt->num_rows;
+                  # Bind results
+                  $stmt->bind_result( $album_id, $album_name );
+                  # Fetch the values into arrays
+                  $i = 0;
+                  while ( $stmt->fetch() ) {
+                    $album_array[] = $album_name; 
+                    $albumID_array[] = $album_id;             
+                    # Review results
+                    if ( DEBUG ) {
+                      echo "USER_ID: ".$_SESSION['user_id'].'<br />';
+                      echo "ALBUM_ID[$i]: $album_id<br />";
+                      echo "ALBUM_NAME[$i]: $album_name<br />";
+                    }
+                    $album_names[$i] = $album_name;
+                    $album_id[$i] = $album_id;
+                    $i++;             
+                  }
+                  unset( $i );
+                  # Cleanup statement
+                  $stmt->close();
+                }
+            }
+        }
+    }
+}
   # debug block
   if ( DEBUG ) {
     echo "Album Name: '$name'<br>";
